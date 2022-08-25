5c3c17132632ddb853881f4f2245ac2a5d9967f5
RyanWatson425 <77034361+RyanWatson425@users.noreply.github.com>
2022-08-25 13:41:29 -0700

    Update testVersium_REACH.php

--- testVersium_REACH.php
+++ testVersium_REACH.php
@@ -5,7 +5,7 @@ include ("Versium_REACH.php");
 
 $inputData = array_fill(0, 30, ["first" => "Angela", "last" => "Adams", "email" => "adamsangela@hotmail.com"]);
 //$inputData[0] = ["first" => "Angela", "last" => "Adams", "email" => "adamsangela@hotmail.com"];
-$apiKey = "949aae79-8415-45a1-8b99-b1ec4ea7c2cc";
+$apiKey = "";
 $outputTypes = ["address", "phone"];
 $dataTool = "contact";
 
