5859d73afd72b9d3c7a86e921ec97e3e2820fd3f
Ryan Watson <rwatson@Ryan-Ws-MacBook-Pro.local>
2022-08-29 13:08:33 -0700

    Added retries, increased customizability, and error logging

--- Versium_REACH.php
+++ Versium_REACH.php
@@ -1,102 +1,151 @@
 <?php
-class VersiumREACH {
+class VersiumREACH
+{
+    private string $apiKey;
+    public int $CURLOPT_CONNECTTIMEOUT;
+    public int $CURLOPT_TIMEOUT;
+    public bool $verbose;
+    public int $waitTime; //microseconds
+
+    function __construct(string $apiKey, int $CURLOPT_CONNECTTIMEOUT = 5, int $CURLOPT_TIMEOUT = 10, bool $verbose = false, int $waitTime = 2000000)
+    {
+        $this->apiKey = $apiKey;
+        $this->CURLOPT_CONNECTTIMEOUT = $CURLOPT_CONNECTTIMEOUT;
+        $this->CURLOPT_TIMEOUT = $CURLOPT_TIMEOUT;
+        $this->verbose = $verbose;
+        $this->waitTime = $waitTime;
+    }
 
     /**
      * This function should be used to effectively query Versium REACH APIs. See our API Documentation for more information
      * https://api-documentation.versium.com/reference/welcome
      *
+     * @param  string $dataTool
+     * the current options for dataTool are: contact, demographic, b2cOnlineAudience, b2bOnlineAudience, firmographic, c2b, iptodomain
      * @param  array  $inputData
      * Each index of the inputData array should contain an array of key value pairs where the keys are the header names and the values are the value of the contact for that specific header
      * ex. $inputData[0] = ["first" => "someFirstName", "last" => "someLastName", "email" => "someEmailAddress"];
      * @param  array  $outputTypes
      * This array should contain a list of strings where each string is a desired output type. This parameter is optional if the API you are using does not require output types
-     * @param  string $apiKey
-     * @param  string $dataTool
-     * the current options for dataTool are: contact, demographic, b2cOnlineAudience, b2bOnlineAudience, firmographic, c2b, iptodomain
      * @return array
      */
-    public static function append(array $inputData, array $outputTypes = [], string $apiKey, string $dataTool) {
-        $requests = array();
-        $Recs = array_fill_keys(array_keys($inputData), '');
+    public function append(string $dataTool, array $inputData, array $outputTypes = [])
+    {
+        $retryUrls = [];
+        $failedRequests = 0;
+        $requests = [];
         $active = 0;
         $multiHandle = curl_multi_init();
-        $urls = array();
+        $urls = [];
         $baseURL = "https://api.versium.com/v2/$dataTool?";
-        if (sizeof($outputTypes) > 0) {
-            foreach($outputTypes as $outputType) {
-                $baseURL .= "output[]=$outputType&";
-            }
+        
+        foreach ($outputTypes as $outputType) {
+            $baseURL .= "output[]=$outputType&";
         }
-        if (sizeof($inputData) > 0) {
-            $counter = 0;
-            foreach($inputData as $row) {
-                $rowInputParams = "";
-                foreach($row as $header => $value) {
-                    $rowInputParams .= "$header=$value&";
-                }
-                $rowInputParams = substr($rowInputParams, 0, -1); //removes last & from the input params
-                $urls[$counter] = $baseURL . $rowInputParams;
+        $counter = 0;
+        foreach ($inputData as $row) {
+            $urls[$counter] = $baseURL . http_build_query($row);
+            if ($this->verbose) {
                 if (isset($urls[$counter])) {
                     echo "urls[$counter]: " . $urls[$counter] . "\n";
                 } else {
                     echo "urls[$counter] is not defined\n";
                 }
-                $counter++;
             }
-        } else {
-            echo "No input data was given\n";
-            return;
+            $counter++;
         }
 
-        foreach ($urls as $i => $url) {
-            $requests[$i] = curl_init();
-            $headers = array(
-                "Accept: application/json",
-                "x-versium-api-key: $apiKey",
-            );
-
-            curl_setopt($requests[$i], CURLOPT_URL, $url);
-            curl_setopt($requests[$i], CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($requests[$i], CURLOPT_RETURNTRANSFER, 1);
-            curl_setopt($requests[$i], CURLOPT_CONNECTTIMEOUT, 5);
-            curl_setopt($requests[$i], CURLOPT_TIMEOUT, 10);
-
-            curl_multi_add_handle($multiHandle, $requests[$i]);
+        if (count($inputData) < 1) {
+            if ($this->verbose) {
+                echo "No input data was given\n";
+            }
+            return [];
         }
 
+        $this->buildRequests($urls, $multiHandle, $requests);
+    
+        //send off all initial requests
         do {
             $status = curl_multi_exec($multiHandle, $active);
-            usleep(10000);
-        } while ($active > 0 && $status == CURLM_OK);
+            while (false !== ($info = curl_multi_info_read($multiHandle))) {
+                if (curl_getinfo($info['handle'], CURLINFO_HTTP_CODE) == 429 || curl_getinfo($info['handle'], CURLINFO_HTTP_CODE) == 500) {
+                    array_push($retryUrls, curl_getinfo($info['handle'], CURLINFO_EFFECTIVE_URL));
 
-        // now get all data and close all requests and return records.
-        foreach ($urls as $i => $url) {
-            $error = null;
-            $errorNum = curl_errno($requests[$i]);
-            $httpStatus = curl_getinfo($requests[$i], CURLINFO_HTTP_CODE);
-
-            $data = curl_multi_getcontent($requests[$i]);
-            
-            $Recs[$i] = [];
-
-            if ($errorNum || $httpStatus != 200) {
-                if ($httpStatus === 0 || $errorNum === 28) { // status:0 == connection lost, curl err 28 == timeout
-                    echo("connection lost or timed out\n");
-                } else {
-                    echo("httpStatus: $httpStatus\n");
-                }
-            } else {
-                if (!empty($data)) {
-                    $Recs[$i] = json_decode($data);
                 }
             }
+            usleep(10000);
+        } while ($active > 0 && $status == CURLM_OK);
+        $numRetriedUrls = count($retryUrls);
 
+        //retry requests that failed the first time after wait time is complete
+        if ($numRetriedUrls > 0) {
+            usleep($this->waitTime);
+            $this->buildRequests($retryUrls, $multiHandle, $requests);
+            do {
+                $status = curl_multi_exec($multiHandle, $active);
+                usleep(10000);
+            } while ($active > 0 && $status == CURLM_OK);
+        }
+
+        //now put all the response data in $recs, close all requests, and return $recs
+        $emptyResponses = 0;
+        for ($i = 0; $i < count($requests) - $emptyResponses; $i++) {
+            $errorNum = curl_errno($requests[$i]);
+            $httpStatus = curl_getinfo($requests[$i], CURLINFO_HTTP_CODE);
+            $data = curl_multi_getcontent($requests[$i]);
+            $headerSize = curl_getinfo($requests[$i], CURLINFO_HEADER_SIZE);
+            $responseHeader = substr($data, 0, $headerSize);
+            $response = substr($data, $headerSize);
             curl_multi_remove_handle($multiHandle, $requests[$i]);
             curl_close($requests[$i]);
+            if ($this->verbose && ($errorNum || $httpStatus != 200)) {
+                if ($httpStatus == 0 || $errorNum == 28) { //status:0 == connection lost, curl err 28 == timeout
+                    echo ("connection lost or timed out\n");
+                } else {
+                    $failedRequests++;
+                }
+            } 
+            if (!empty($response)) {
+                $recs[$i] = [
+                    "response" => json_decode($response),
+                    "http_status" => $httpStatus,
+                    "response_header" => $responseHeader
+                ];
+            }
         }
-        // now close the multi get handle
+
         curl_multi_close($multiHandle);
 
-        return $Recs;
+        if ($this->verbose) {
+            print_r($recs);
+            print_r("Number of records submitted to the append function: " . count($urls) . "\n");
+            print_r("Number of requests that failed on initial attempt due to 429 or 500 error code: " . $numRetriedUrls . "\n");
+            print_r("Number of requests that were successfully retried: " . ($numRetriedUrls - $failedRequests) . "\n");
+            print_r("Number of requests that failed (on both initial and retry attempt): ". $failedRequests . "\n");
+            print_r("Number of response records returned: " . count($recs) . "\n");
+        }
+        return $recs;
+    }
+
+    private function buildRequests(array $urls, &$multiHandle, array &$requests)
+    {
+        $requestLength = count($requests) == 0 ? count($requests) : count($requests) - count($urls); //prevents writing over original requests
+        foreach ($urls as $i => $url) {
+            $requests[$i + $requestLength] = curl_init();
+            $headers = array(
+                "Accept: application/json",
+                "x-versium-api-key: " . $this->apiKey,
+            );
+
+            curl_setopt($requests[$i + $requestLength], CURLOPT_URL, $url);
+            curl_setopt($requests[$i + $requestLength], CURLOPT_HTTPHEADER, $headers);
+            curl_setopt($requests[$i + $requestLength], CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($requests[$i + $requestLength], CURLOPT_CONNECTTIMEOUT, $this->CURLOPT_CONNECTTIMEOUT);
+            curl_setopt($requests[$i + $requestLength], CURLOPT_TIMEOUT, $this->CURLOPT_TIMEOUT);
+            curl_setopt($requests[$i + $requestLength], CURLOPT_HEADER, true);
+            curl_setopt($requests[$i + $requestLength], CURLOPT_FAILONERROR, true);
+
+            curl_multi_add_handle($multiHandle, $requests[$i + $requestLength]);
+        }
     }
 }
--- testVersium_REACH.php
+++ testVersium_REACH.php
@@ -3,14 +3,12 @@
 /* THIS FILE IS JUST FOR TESTING - MAKE SURE TO DELETE ONCE SDK IS COMPLETE */
 include ("Versium_REACH.php");
 
-$inputData = array_fill(0, 30, ["first" => "Angela", "last" => "Adams", "email" => "adamsangela@hotmail.com"]);
+$inputData = array_fill(0, 40, ["first" => "Angela", "last" => "Adams", "email" => "adamsangela@hotmail.com"]);
 //$inputData[0] = ["first" => "Angela", "last" => "Adams", "email" => "adamsangela@hotmail.com"];
 $apiKey = "949aae79-8415-45a1-8b99-b1ec4ea7c2cc";
 $outputTypes = ["address", "phone"];
 $dataTool = "contact";
 
-$Versium_REACH = new VersiumREACH();
+$Versium_REACH = new VersiumREACH($apiKey, 5, 10, true);
 
-$responses = $Versium_REACH->append($inputData, $outputTypes, $apiKey, $dataTool);
-
-print_r($responses);
+$responses = $Versium_REACH->append($dataTool, $inputData, $outputTypes);
